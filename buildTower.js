edab6a089b025a10186ed1feeb753b5faa22230b
TasukuY <alohaty81@gmail.com>
2022-03-14 10:28:29 -0600

    added other solutions

--- buildTower.js
+++ buildTower.js
@@ -1,22 +1,22 @@
 
-function createTower(floors){
-    let tower = [];
-    let numOfAsterisk = rowSize(floors);
-    const rowNum = rowSize(floors);
-    while(numOfAsterisk > 0){
-        let row = '';
-        let numOfSpaces = rowNum - numOfAsterisk;
-        for(let i = 0; i < numOfAsterisk; i++){
-            row += '*'
-        }
-        for(let i = 0; i < numOfSpaces/2; i++){
-            row = ' ' + row + ' ';
-        }
-        tower.push(row);
-        numOfAsterisk -= 2;
-    }
-    return tower.reverse();
-}
+// function createTower(floors){
+//     let tower = [];
+//     let numOfAsterisk = rowSize(floors);
+//     const rowNum = rowSize(floors);
+//     while(numOfAsterisk > 0){
+//         let row = '';
+//         let numOfSpaces = rowNum - numOfAsterisk;
+//         for(let i = 0; i < numOfAsterisk; i++){
+//             row += '*'
+//         }
+//         for(let i = 0; i < numOfSpaces/2; i++){
+//             row = ' ' + row + ' ';
+//         }
+//         tower.push(row);
+//         numOfAsterisk -= 2;
+//     }
+//     return tower.reverse();
+// }
 
 let rowSize = floors => {
     let num = 1;
@@ -27,4 +27,16 @@ let rowSize = floors => {
     return num;
 }
 
-console.log(createTower(6));
+towers2 = function(layers) {
+    tree = []; 
+    for (i = layers; i > 0; i--){
+        aster = '*'.repeat(i * 2 - 1);
+        spacez = ' '.repeat(layers - i);
+        finl = spacez+aster+spacez;tree.unshift(finl);
+    }
+    return tree;
+}
+
+console.log(towers2(6))
+
+// console.log(createTower(6));
